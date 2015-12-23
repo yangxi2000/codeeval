e7250b3f9cc0ac8528c50459138368b385a1c303
yangxi2000 <yangxi2000@gmail.com>
2015-12-24 09:06:59 +1100

    Create output.py

---
+++ easy/multiply_lists/output.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""MULTIPLY LISTS
+
+CHALLENGE DESCRIPTION:
+
+You have 2 lists of positive integers. Write a program which multiplies corresponding elements in these lists.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. Input example is the following
+
+9 0 6 | 15 14 9
+5 | 8
+13 4 15 1 15 5 | 1 4 15 14 8 2
+The lists are separated with a pipe char, numbers are separated with a space char. 
+The number of elements in lists are in range [1, 10]. 
+The number of elements is the same in both lists. 
+Each element is a number in range [0, 99].
+
+OUTPUT SAMPLE:
+
+Print the result in the following way.
+
+135 0 54
+40
+13 16 225 14 120 10
+"""
+
+import sys
+import re
+
+def main():
+	
+    try:
+        test_cases = open(sys.argv[1], 'r')
+    except:
+        sys.exit()
+
+    for test in test_cases:
+        tests = test.strip().split(' | ')
+        a = tests[0].split(' ')
+        b = tests[1].split(' ')
+        zipped = zip(a, b)
+        
+        for i in zipped:
+            print int(i[0]) * int(i[1]),
+        print('')
+        
+    test_cases.close()
+
+if __name__ == "__main__":
+    main()
